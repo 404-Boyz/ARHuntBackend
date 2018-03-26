68204481870c19468f2d8c68b7fc3b49d3d20d9b
Wayne Temple <waynetemple@gmail.com>
2018-03-26 12:14:45 -0500

    so close

--- server/api/adventure.js
+++ server/api/adventure.js
@@ -24,18 +24,16 @@ router.get('/:adventureId/location', (req, res, next) => {
 });
 
 
-// router.get('/:adventureId/location/:locationId/active', (req, res, next) => {
-//     Location.findAll({
-//         where: {
-//             adventureId: req.params.adventureId,
-//             id: req.params.locationId
-//         },
-//         returning: true,
-//         plain: true
-//     })
-//         .then(location => res.json(location[1].dataValues))
-//         .catch(next)
-// });
+router.get('/:adventureId/location/:locationId/active', (req, res, next) => {
+    Location.findAll({
+        where: {
+            adventureId: req.params.adventureId,
+            id: req.params.locationId
+        },
+    })
+        .then(location => res.json(location[1].dataValues))
+        .catch(next)
+});
 
 router.put('/:adventureId/location/:locationId/visited', (req, res, next) => {
     return Location.update({ visited: req.body.visited, active: false }, {
