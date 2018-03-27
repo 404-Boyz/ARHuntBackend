05e72e1b362174a219cdd900ee606d7ffbee0ee3
Tom Sziler <tom.sziler@gmail.com>
2018-03-27 10:36:47 -0500

    adventureActive

--- server/api/adventure.js
+++ server/api/adventure.js
@@ -18,10 +18,12 @@ router.put('/:adventureId', (req, res, next) => {
     return Adventure.update({ status: req.body.status }, {
         where: {
             userId: req.body.userId,
-            adventureId: req.body.adventureId,
-        }
+            id: req.body.id,
+        },
+        returning: true,
+        plain: true
     })
-        .then(location =>  res.json(location))
+        .then(location =>  res.json(location[1].dataValues))
         .catch(next)
 });
 
@@ -62,14 +64,16 @@ router.get('/:adventureId/location/:locationId', (req, res, next) => {
         .catch(next)
 });
 
-router.put('/:adventureId/location/:locationId', (req, res, next) => {
+router.put('/:adventureId/location/:locationId/visited', (req, res, next) => {
     return Location.update({ visited: req.body.visited }, {
         where: {
             adventureId: req.body.adventureId,
             id: req.body.id
-        }
+        },
+        returning: true,
+        plain: true
     })
-        .then(location =>  res.json(location))
+        .then(location =>  res.json(location[1].dataValues))
         .catch(next)
 });
 
